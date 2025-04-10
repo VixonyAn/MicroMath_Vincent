73b7e25f31d05857ca1973c8895d424638e71e9e
VixonyAn <vixonyan@gmail.com>
2025-04-10 10:41:37 +0200

    Added comment to Add function

--- MicroMath_Vincent/MicroMath_Vincent.cs
+++ MicroMath_Vincent/MicroMath_Vincent.cs
@@ -8,6 +8,12 @@ namespace MicroMath_Vincent
 {
     public class MicroMath_Vincent
     {
+        /// <summary>
+        /// Lægger to tal sammen
+        /// </summary>
+        /// <param name="a">Tal 1</param>
+        /// <param name="b">Tal 2</param>
+        /// <returns>Int: Sum af tal</returns>
         public int Add(int a, int b)
         {
             return a + b;
